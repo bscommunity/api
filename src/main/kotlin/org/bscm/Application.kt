9ef10e94eae87adef345e77b65a22bd49865e1ef
meninocoiso <225307799+meninocoiso@users.noreply.github.com>
2025-01-14 22:34:51 -0300

    🧱 base: initial sample code

---
+++ src/main/kotlin/org/bscm/Application.kt
@@ -0,0 +1,15 @@
+package org.bscm
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureSecurity()
+    configureHTTP()
+    configureRouting()
+}
