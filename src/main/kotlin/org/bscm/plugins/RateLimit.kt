92d347f798336ca137f55ffabd3adb99feaaf46a
meninocoiso <225307799+meninocoiso@users.noreply.github.com>
2025-06-02 01:08:12 -0300

    🔗 fix: improved error handling and rate limiting

--- src/main/kotlin/org/bscm/plugins/RateLimit.kt
+++ src/main/kotlin/org/bscm/plugins/RateLimit.kt
@@ -7,7 +7,7 @@ import kotlin.time.Duration.Companion.seconds
 fun Application.configureRateLimit() {
     install(RateLimit) {
         global {
-            rateLimiter(limit = 5, refillPeriod = 22.5.seconds)
+            rateLimiter(limit = 5, refillPeriod = 30.seconds)
         }
         register(RateLimitName("protected")) {
             rateLimiter(limit = 30, refillPeriod = 60.seconds)
--- src/main/kotlin/org/bscm/plugins/StatusPage.kt
+++ src/main/kotlin/org/bscm/plugins/StatusPage.kt
@@ -12,11 +12,13 @@ fun Application.configureStatusPages() {
 
     install(StatusPages) {
         status(HttpStatusCode.NotFound) { call, status ->
-            call.respondText(text = "404: Page Not Found", status = status)
+            // call.respondText(text = "404: Page Not Found", status = status)
+            call.respond(HttpStatusCode.NotFound, mapOf("message" to "Page not found"))
         }
         status(HttpStatusCode.TooManyRequests) { call, status ->
-            val retryAfter = call.response.headers["Retry-After"]
-            call.respondText(text = "Whoa there! You're going way too fast \uD83D\uDEA6. Try again in $retryAfter seconds.", status = status)
+            // val retryAfter = call.response.headers["Retry-After"]
+            // call.respondText(text = "Whoa there! You're going way too fast \uD83D\uDEA6. Try again in $retryAfter seconds.", status = status)
+            call.respond(HttpStatusCode.TooManyRequests, mapOf("message" to "Too many requests. Please slow down."))
         }
         exception<Throwable> { call, cause ->
             logger.error("An unexpected error occurred", cause)
